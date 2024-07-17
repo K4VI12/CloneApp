5b62c557fc3b3ab6c191e35d8d2808b0451d2656
Kavindu Hansaka <kavinduhansaka755@gmail.com>
2024-07-18 02:07:52 +0530

    commit

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,16 +2,25 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../../constants/Colors';
 import products from '../../../assets/data/products';
 
-const product = products[0];
+const product = products[3];
 
-export default function TabOneScreen() {
-  return (
+const ProductListItem = () =>{
+  return(
     <View style={styles.container}>
       <Image source={{ uri: product.image }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
   );
+};
+
+export default function TabOneScreen() {
+  return (
+  <View>
+    <ProductListItem/>
+    <ProductListItem/>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
