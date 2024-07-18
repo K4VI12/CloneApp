1ac8d809003fb2182d8a7ada0a8c6fa7e21e28cd
Kavindu Hansaka <kavinduhansaka755@gmail.com>
2024-07-18 19:01:48 +0530

    flex and image size fix

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,16 +3,22 @@ import Colors from '../constants/Colors';
 import { Product } from '../types';
 
 export const defaultPizzaImage =
-'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProductListItemProps = {
   product: Product;
 }
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
-  return(
+  return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }} 
+        style={styles.image}
+        resizeMode='contain'
+      />
+
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -26,7 +32,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
+
   image: {
     width: '100%',
     aspectRatio: 1,
